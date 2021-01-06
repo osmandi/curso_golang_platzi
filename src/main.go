79e68ea92a7075e2bbb320e5375c6b1c1a61d7b8
Osmandi <osmandigt@gmail.com>
2021-01-06 09:16:03 -0500

    Clean code

--- src/main.go
+++ src/main.go
@@ -1,46 +1,5 @@
 package main
 
-import "fmt"
-
 func main() {
-
-	// Area cuadrado
-	const baseCuadrado = 10
-	areaCuadrado := baseCuadrado * baseCuadrado
-	fmt.Println("Area cuadrado:", areaCuadrado)
-
-	x := 10
-	y := 50
-
-	// Suma
-	result := x + y
-	fmt.Println("Suma:", result)
-
-	// Resta
-	result = y - x
-	fmt.Println("Resta:", result)
-
-	// Multiplicación
-	result = x * y
-	fmt.Println("Multiplicación:", result)
-
-	// División
-	result = y / x
-	fmt.Println("División:", result)
-
-	// Modulo
-	result = y % x
-	fmt.Println("Modulo:", result)
-
-	// Incremental
-	x++
-	fmt.Println("Incremental:", x)
-
-	// Decremental
-	x--
-	fmt.Println("Decremental:", x)
-
-	// Retos
-	// -Rectángulo, trapecio y de un círculo
 
 }
