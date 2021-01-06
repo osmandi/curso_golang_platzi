240173ea92c4b9bde1add4cb384160424968278c
Osmandi <osmandigt@gmail.com>
2021-01-06 11:49:03 -0500

    Finished class

--- src/main.go
+++ src/main.go
@@ -1,5 +1,26 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	// Defer
+	defer fmt.Println("Hola")
+	fmt.Println("Mundo")
+
+	// Continue y break
+	for i := 0; i < 10; i++ {
+		fmt.Println(i)
+
+		// Continue
+		if i == 2 {
+			fmt.Println("Es 2")
+			continue
+		}
+
+		// Break
+		if i == 8 {
+			fmt.Println("Break")
+			break
+		}
+	}
 }
