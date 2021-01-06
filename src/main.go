d92b5b0d38d41d23a1cbf1c9831ea72a375cb356
Osmandi <osmandigt@gmail.com>
2021-01-06 08:16:31 -0500

    Clean environment

--- src/main.go
+++ src/main.go
@@ -3,27 +3,6 @@ package main
 import "fmt"
 
 func main() {
-	// Declaración de constantes
-	const pi float64 = 3.14
-	const pi2 = 3.1415
-
-	fmt.Println("pi:", pi)
-	fmt.Println("pi2:", pi2)
-
-	// Declaración de variables enteras
-	base := 12
-	var altura int = 14
-	var area int
-
-	fmt.Println(base, altura, area)
-
-	// Zero values
-	var a int
-	var b float64
-	var c string
-	var d bool
-
-	fmt.Println(a, b, c, d)
 
 	// Area cuadrado
 	const baseCuadrado = 10
