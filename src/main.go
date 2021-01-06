20ef3ccaeee0950ff21e4fa4a0f486377cc38112
Osmandi <osmandigt@gmail.com>
2021-01-06 09:14:45 -0500

    Finished class

--- src/main.go
+++ src/main.go
@@ -8,4 +8,39 @@ func main() {
 	const baseCuadrado = 10
 	areaCuadrado := baseCuadrado * baseCuadrado
 	fmt.Println("Area cuadrado:", areaCuadrado)
+
+	x := 10
+	y := 50
+
+	// Suma
+	result := x + y
+	fmt.Println("Suma:", result)
+
+	// Resta
+	result = y - x
+	fmt.Println("Resta:", result)
+
+	// Multiplicación
+	result = x * y
+	fmt.Println("Multiplicación:", result)
+
+	// División
+	result = y / x
+	fmt.Println("División:", result)
+
+	// Modulo
+	result = y % x
+	fmt.Println("Modulo:", result)
+
+	// Incremental
+	x++
+	fmt.Println("Incremental:", x)
+
+	// Decremental
+	x--
+	fmt.Println("Decremental:", x)
+
+	// Retos
+	// -Rectángulo, trapecio y de un círculo
+
 }
