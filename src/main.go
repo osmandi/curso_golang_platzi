798ff8bb561b5800bd3094c0b0b6945ab19792c5
Osmandi <osmandigt@gmail.com>
2021-01-06 10:31:39 -0500

    Set environment

--- src/main.go
+++ src/main.go
@@ -1,27 +1,5 @@
 package main
 
-import "fmt"
-
 func main() {
 
-	// For condicional
-	for i := 0; i <= 10; i++ {
-		fmt.Println(i)
-	}
-
-	fmt.Printf("\n")
-
-	// For while
-	counter := 0
-	for counter < 10 {
-		fmt.Println(counter)
-		counter++
-	}
-
-	// For forever
-	counterForever := 0
-	for {
-		fmt.Println(counterForever)
-		counterForever++
-	}
 }
