6708ad633e8263911dad5f34a8684eb88d681359
Osmandi <osmandigt@gmail.com>
2021-01-06 11:01:01 -0500

    Class finished, but require challenges solving

--- src/main.go
+++ src/main.go
@@ -1,5 +1,35 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"strconv"
+)
 
+func main() {
+	valor1 := 1
+	valor2 := 2
+
+	if valor1 == 1 {
+		fmt.Println("Es 1")
+	} else {
+		fmt.Println("No es 1")
+	}
+
+	// With and
+	if valor1 == 1 && valor2 == 3 {
+		fmt.Println("Es verdad")
+	}
+
+	// With or
+	if valor1 == 0 || valor2 == 2 {
+		fmt.Println("Es verdad, OR")
+	}
+
+	// Convertir texto a número
+	value, err := strconv.Atoi("53")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Value:", value)
 }
