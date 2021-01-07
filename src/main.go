e3c0b311e80b4ae5b38a0a69e2002f678bff3887
Osmandi <osmandigt@gmail.com>
2021-01-07 12:15:01 -0500

    Set environment

--- src/main.go
+++ src/main.go
@@ -1,32 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"sync"
-	"time"
-)
-
-func say(text string, wg *sync.WaitGroup) {
-
-	defer wg.Done()
-
-	fmt.Println(text)
-}
-
 func main() {
-	var wg sync.WaitGroup
-
-	fmt.Println("Hello")
-	wg.Add(1)
-
-	go say("world", &wg)
-
-	wg.Wait()
-
-	go func(text string) {
-		fmt.Println(text)
-	}("Adios")
-
-	time.Sleep(time.Second * 1)
 
 }
