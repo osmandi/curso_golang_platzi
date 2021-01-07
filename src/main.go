ae5d14f6b8107a986333ec8eb9a3f9fbabf54b11
Osmandi <osmandigt@gmail.com>
2021-01-07 09:25:45 -0500

    Clean environment

--- src/main.go
+++ src/main.go
@@ -1,18 +1,5 @@
 package main
 
-import (
-	pk "curso_golang_platzi/src/mypackage"
-	"fmt"
-)
-
 func main() {
-	var myCar pk.CarPublic
-	myCar.Brand = "Ferrari"
-	myCar.Year = 2020
-	fmt.Println(myCar)
-
-	pk.PrintMessage("Hola Platzi")
-
-	pk.printMessage1("Hola")
 
 }
