3c8e35fefb261be1300dd7f76c2140aa0df5a9ca
Osmandi <osmandigt@gmail.com>
2021-01-07 08:08:29 -0500

    Clean environment

--- src/main.go
+++ src/main.go
@@ -1,35 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"strings"
-)
-
-func isPalindromo(text string) {
-	var textReverse string
-
-	// Convert text to lower
-	text = strings.ToLower(text)
-
-	for i := len(text) - 1; i >= 0; i-- {
-		textReverse += string(text[i])
-	}
-
-	if text == textReverse {
-		fmt.Println("Es palindromo")
-	} else {
-		fmt.Println("No es un palíndromo")
-	}
-}
-
 func main() {
-	slice := []string{"hola", "que", "hace"}
 
-	for i := range slice {
-		fmt.Println(i)
-	}
-	// ama
-	// amor a roma
-
-	isPalindromo("Ama")
 }
