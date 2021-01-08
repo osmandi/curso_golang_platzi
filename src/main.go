27b1ff38a94053b087373b3d1188981fc1eca82b
Osmandi <osmandigt@gmail.com>
2021-01-08 07:43:52 -0500

    Finished class

--- src/main.go
+++ src/main.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import "fmt"
 
+func say(text string, c chan<- string) {
+	c <- text
+}
+
+func main() {
+	c := make(chan string, 1)
+
+	fmt.Println("Hello")
+
+	go say("Bye", c)
+
+	fmt.Println(<-c)
 }
