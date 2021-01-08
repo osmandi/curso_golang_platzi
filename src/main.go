3657b8887139aa646959b29c77f735f397889be3
Osmandi <osmandigt@gmail.com>
2021-01-08 08:07:16 -0500

    Finished class

--- src/main.go
+++ src/main.go
@@ -1,5 +1,37 @@
 package main
 
-func main() {
+import "fmt"
 
+func message(text string, c chan string) {
+	c <- text
+}
+
+func main() {
+	c := make(chan string, 2)
+	c <- "Mensaje1"
+	c <- "Mensaje2"
+
+	fmt.Println(len(c), cap(c))
+
+	// Range y close
+	close(c)
+
+	//c <- "Mensaje3"
+	for message := range c {
+		fmt.Println(message)
+	}
+
+	// Select
+	email1 := make(chan string)
+	email2 := make(chan string)
+	go message("mensaje1", email1)
+	go message("mensaje2", email2)
+	for i := 0; i < 2; i++ {
+		select {
+		case m1 := <-email1:
+			fmt.Println("Email recibido de email1", m1)
+		case m2 := <-email2:
+			fmt.Println("Email recibido de email2", m2)
+		}
+	}
 }
