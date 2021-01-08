2e70dc0450b9ab900b3eca35039db91cba97cb92
Osmandi <osmandigt@gmail.com>
2021-01-08 09:47:30 -0500

    Finished class

--- src/main.go
+++ src/main.go
@@ -1,19 +1,18 @@
 package main
 
-import (
-	"net/http"
-
-	"github.com/labstack/echo"
-)
+import "fmt"
 
 func main() {
+	/*
 
-	// Instanciar echo
-	e := echo.New()
+		// Instanciar echo
+		e := echo.New()
 
-	// Ruta
-	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello world")
-	})
-	e.Logger.Fatal(e.Start(":1323"))
+		// Ruta
+		e.GET("/", func(c echo.Context) error {
+			return c.String(http.StatusOK, "Hello world")
+		})
+		e.Logger.Fatal(e.Start(":1323"))
+	*/
+	fmt.Println("Hello")
 }
