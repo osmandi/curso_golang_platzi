2c49da5294cc94371f2736c385b55cd64b0ca0e7
Osmandi <osmandigt@gmail.com>
2021-01-08 09:16:28 -0500

    Finished class

---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/osmandi/curso_golang_platzi
+
+go 1.15
+
+require (
+	github.com/labstack/echo v3.3.10+incompatible // indirect
+	github.com/labstack/gommon v0.3.0 // indirect
+	golang.org/x/crypto v0.0.0-20201221181555-eec23a3978ad // indirect
+)
--- src/main.go
+++ src/main.go
@@ -1,4 +1,19 @@
 package main
 
+import (
+	"net/http"
+
+	"github.com/labstack/echo"
+)
+
 func main() {
+
+	// Instanciar echo
+	e := echo.New()
+
+	// Ruta
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello world")
+	})
+	e.Logger.Fatal(e.Start(":1323"))
 }
