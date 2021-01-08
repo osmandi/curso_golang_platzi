bf58e6e0ed59c843be4b801e0f5c7b93b8c19d8e
Osmandi <osmandigt@gmail.com>
2021-01-08 07:44:43 -0500

    Clean environment

--- src/main.go
+++ src/main.go
@@ -1,17 +1,5 @@
 package main
 
-import "fmt"
-
-func say(text string, c chan<- string) {
-	c <- text
-}
-
 func main() {
-	c := make(chan string, 1)
 
-	fmt.Println("Hello")
-
-	go say("Bye", c)
-
-	fmt.Println(<-c)
 }
