70843ee4e8322674a66304baada8896c5ff514d2
Osmandi <osmandigt@gmail.com>
2021-01-18 08:43:22 -0500

    Add challenge

--- src/main.go
+++ src/main.go
@@ -6,6 +6,26 @@ import (
 	"strconv"
 )
 
+func esPar(value int) bool {
+	if module := value % 2; module == 0 {
+		return true
+	}
+	return false
+}
+
+func getLogin(username, password string) {
+	/* Esta función compara el username y el password y verificar si coincide
+	   hacer login de esta manera (username y password en el código), es una muy mala práctica
+	   usualmente los frameworks utilizan librerías para hacer este check
+	   con passwords encriptados desde la base de datos.
+	*/
+	if username == "platzi" && password == "platzi" {
+		fmt.Println("You are logged")
+	} else {
+		fmt.Println("Username or password incorrect")
+	}
+}
+
 func main() {
 	valor1 := 1
 	valor2 := 2
@@ -32,4 +52,14 @@ func main() {
 		log.Fatal(err)
 	}
 	fmt.Println("Value:", value)
+
+	// Evaluar número par e impar
+	fmt.Println(esPar(2))
+	fmt.Println(esPar(3))
+
+	// Función para hacer login
+	username := "platzi"
+	password := ""
+	getLogin(username, password)
+
 }
